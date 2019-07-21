38d2857c35a8793300b7b15a162b29ff095a9f7f
Narasimha Reddy <macbook@APPLEs-MacBook-Air.local>
2019-07-22 00:01:36 +0530

    action creators

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export function movieslist(){
+    return {
+        type : 'MOVIES_LIST',
+        payload : [
+            {id:'1',name:'The man with golden guns'},
+            {id:'2',name:'God must be crazy'},
+            {id:'3',name:'Zoltan'},
+            {id:'4',name:'Dude, where is my car?'}
+        ]
+    }
+}
--- src/reducers/movie_reducer.js
+++ src/reducers/movie_reducer.js
@@ -15,7 +15,7 @@
 export default function(state={},action){
     switch(action.type){
         case 'MOVIES_LIST':
-            return {...state,movies=action.payload}
+            return {...state,movies:action.payload}
         default:
             return state;
     }
